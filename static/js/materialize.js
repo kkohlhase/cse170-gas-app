960721e8730c7de70e5d700ff4b2440d0ec6b8cd
Gabe Maze-Rogers <gmazerogers@gmail.com>
2016-02-03 23:33:45 -0800

    Added modal for Profile with slider template. Need to think about what functionality is needed for Profile/Settings Page

--- static/js/materialize.js
+++ static/js/materialize.js
@@ -13,7 +13,18 @@ if (typeof(jQuery) === 'undefined') {
   } else {
     jQuery = $;
   }
-};/*
+};
+
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+
+/*
+
+
+
  * jQuery Easing v1.3 - http://gsgd.co.uk/sandbox/jquery/easing/
  *
  * Uses the built in easing capabilities added In jQuery 1.1
